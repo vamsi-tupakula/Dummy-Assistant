d2824e2456b2c8e3a78fc943efbf400bdba082fc
Vamsi <vamsitupakula474@gmail.com>
2021-11-14 20:51:02 +0530

    text to speech added

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
 
-try:
-    with sr.Microphone() as source:
-        print("Say something!....")
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        print(command)
-except:
-    print("sorry....")
+speaker = pyttsx3.init()
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice',voices[1].id)
+
+def speak():
+    speaker.say("I am your alexa")
+    speaker.say("How can i help you")
+    speaker.runAndWait()
+
+def get_command():
+    try:
+        with sr.Microphone() as source:
+            print("Say something!....")
+            voice = listener.listen(source)
+            command = ''
+            command = listener.recognize_google(voice)
+            if command != '':
+                command = command.lower()
+                if 'alexa' in command:
+                    command = command.replace('alexa','')
+    except:
+        print("sorry....")
+
+speak()
