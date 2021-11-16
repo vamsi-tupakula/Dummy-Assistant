0adb0146ac462fdfc0e94de50b44b342042a20e5
Vamsi <vamsitupakula474@gmail.com>
2021-11-16 10:35:21 +0530

    speak func updated

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ speaker = pyttsx3.init()
 voices = speaker.getProperty('voices')
 speaker.setProperty('voice',voices[1].id)
 
-def speak():
-    speaker.say("I am your alexa")
-    speaker.say("How can i help you")
+def speak(text):
+    speaker.say(text)
     speaker.runAndWait()
 
 def get_command():
@@ -25,5 +24,11 @@ def get_command():
                     command = command.replace('alexa','')
     except:
         print("sorry....")
+    return command
 
-speak()
+def run_alexa():
+    command = get_command()
+    if 'play' in command:
+        command = command.replace('play','')
+        speak('playing' + command)
+
